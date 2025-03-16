5ccf6be5a143adf43c9f955770278f21a91cb4b2
Hex2424 <markas.vielavicius@gmail.com>
2025-03-16 18:01:34 +0200

    moved some stuff from global config to platform specific config

--- utility/global_config/global_config.h
+++ utility/global_config/global_config.h
@@ -13,7 +13,7 @@
 
 #ifndef UTILITY_GLOBAL_CONFIG_GLOBAL_CONFIG_H_
 #define UTILITY_GLOBAL_CONFIG_GLOBAL_CONFIG_H_
-
+#include <platform_specific.h>
 
 /**
  * @brief Setting up development logging verbose level
@@ -22,30 +22,17 @@
  * 2 - (ERROR / WARN)
  * 3 - (INFO / DEBUG / ERROR / WARN)
  */
-#define VERBOSE_LEVEL                   2
+#define VERBOSE_LEVEL                   3
 #define VERBOSE_C_COMPILER              1
 #define ENABLE_TEMP_FILES_CLEANUP       1
 
-// OS types     
-#define WINDOWS                         1
-#define LINUX                           2
-
-#define OS                              LINUX
 
 #define OBJECT_ID_LENGTH                8              // in bytes
 #define MAX_RETRIES_ID                  15
 #define OBJECT_RANDOM_SEED              0
 
-#define LINUX_TEMP_FOLDER_PATH          "./"
-#define WINDOWS_TEMP_FOLDER_PATH        "ECHO %Temp%" // get from CMD
 #define MAIN_PROCESS_FILE_NAME          "___start"
 
-
-#if OS == LINUX
-    #define TEMP_PATH                   LINUX_TEMP_FOLDER_PATH
-#elif OS == WINDOWS
-    #define TEMP_PATH                   WINDOWS_TEMP_FOLDER_PATH
-#endif
 
 #define CFILES_LENGTH                   (sizeof(TEMP_PATH) + OBJECT_ID_LENGTH + sizeof(".x")) - 1
 
